1d666f4f8e0d4aab4cda67e39f488418d9ae3dff
michellekim622 <62683110+michellekim622@users.noreply.github.com>
2020-05-19 18:01:39 -0400

    Update shippingcalculator.py

--- shippingcalculator.py
+++ shippingcalculator.py
@@ -1,6 +1,35 @@
 # shipping calculator
 
-def shippingCalculator(num_items):
-    return (num_items - 1) * 2.95 + 10.95
+def wrong_input_handling(data, variable=''):
+    while True:
+        try:
+            data = float(data)
+        except ValueError:
+            print("Entered unrecognizable as a number %s" % variable )
+            data = input("Enter a positive number: ")
+            continue
+        if data <= 0:
+            print("Entered zero or negative number are not accepted %s" % variable)
+            data = input("Enter a positive number: ")
+            continue
+        elif data != int(data):
+            print("Entered decimal as a number is not accepted %s" % variable )
+            data = input("Enter a positive integer: ")
+            continue
+        else:
+            data = int(data)
+            break
+        
+    return data
 
-print("Total: $", shippingCalculator(10))
+def sendingCost(amountItems, costItem=2.95, costFirst=10.95):
+    
+    amountItems = wrong_input_handling(amountItems, '(Cantidad de Elementos)')    
+    
+    totalCost = costFirst + costItem*(amountItems-1)
+    
+    return totalCost
+
+cost = input("Enter the quantity of items purchased: ")
+
+print("Shipping cost: %f" %sendingCost(cost))
